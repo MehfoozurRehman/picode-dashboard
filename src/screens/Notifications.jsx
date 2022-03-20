280c505e549c2e11b001eca828f3e9d07d8cfc23
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-03-20 18:01:18 +0500

    payment history listing done

--- src/screens/Notifications.jsx
+++ src/screens/Notifications.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-import { Search } from "react-feather";
-import { Link } from "react-router-dom";
 import TableHeaderEntry from "../components/TableHeaderEntry";
-import { Edit, Trash } from "react-feather";
 
 function NotificationsTableEntry({ onEdit, onDelete }) {
   return (
